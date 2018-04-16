64a544ef19407de1e12db9c10ffeea3c90ab2d20
Irmantas Čivilis <irmantas.civilis@gmail.com>
2018-04-16 11:24:14 +0300

    Add recycler view adapter

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -4,57 +4,60 @@ import android.app.LoaderManager;
 import android.content.Loader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
-import android.widget.Toast;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Movie>>{
-
     public static final String LOG_TAG = MainActivity.class.getName();
     private static final int MOVIE_LOADER_ID = 1;
 
     private static final String API_KEY = "";
-    private static final String TMDB_REQUEST_URL = "http://api.themoviedb.org/3/movie/";
+    private static final String TMDB_REQUEST_URL = "https://api.themoviedb.org/3/movie/";
     private static final String POPULAR = "popular?";
     private static final String TOP_RATED = "top_rated?";
 
     String movieUrl = "";
 
     String posterPath = "";
-    String imageSize = "w185";
-    private static final String BASE_IMAGE_URL = "http://image.tmdb.org/t/p/";
+    String imageSize = "w185/";
+    private static final String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/";
+
+    private static final int NUM_LIST_ITEMS = 100;
 
     MovieAdapter mAdapter;
+    Boolean isConnected;
+    List<Movie> mMovies;
+    TextView textView;
+    ImageView imageView;
+
+    //private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GridView gridView = findViewById(R.id.gridview);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
 
+        mLayoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setHasFixedSize(true);
         mAdapter = new MovieAdapter(this, new ArrayList<Movie>());
-        gridView.setAdapter(mAdapter);
-
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT)
-                        .show();
-            }
-        });
+        recyclerView.setAdapter(mAdapter);
 
         movieUrl = TMDB_REQUEST_URL + POPULAR + API_KEY;
 
         LoaderManager loaderManager = getLoaderManager();
+        loaderManager.initLoader(MOVIE_LOADER_ID, null, MainActivity.this);
 
-        loaderManager.initLoader(MOVIE_LOADER_ID, null, this);
+
     }
 
     @Override
@@ -65,13 +68,28 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoadFinished(Loader<List<Movie>> loader, List<Movie> movies) {
-        mAdapter.clear();
-        mAdapter.addAll(movies);
 
+        if (movies != null && !movies.isEmpty()) {
+            mAdapter.setMovieData(movies);
+        }
     }
 
     @Override
     public void onLoaderReset(Loader loader) {
-        mAdapter.clear();
     }
+
+    //public boolean checkConnectivity() {
+       // ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        //NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        //isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+        //return isConnected;
+    //}
+    //TODO 1. Pridėti meniu su pasirinkimu top rated paieška
+    //TODO 2. SUkurti detail activity ir layoutą
+    //TODO 3.  Sujungti activities intentu
+    //TODO 4. onclick listenerį uždėti ant posterių
+    //TODO 5. Patobulinti UI, pridėti loadinimo indikatorių.
+    //TODO 6. Pagalvoti dėl landscape mode
+    //TODO 7.
+
 }
--- app/src/main/java/com/example/android/popularmovies/MovieAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/MovieAdapter.java
@@ -1,47 +1,66 @@
 package com.example.android.popularmovies;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
-public class MovieAdapter extends ArrayAdapter<Movie> {
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
 
-    private Context mContext = getContext();
-    List<Movie> mMovies;
-    String posterPath = "";
-    String imageSize = "w185";
-    private static final String BASE_IMAGE_URL = "http://image.tmdb.org/t/p/";
+    private static final String TAG = MovieAdapter.class.getSimpleName();
+    private List<Movie> mMovies;
+    private LayoutInflater mInflater;
+    private AdapterView.OnItemClickListener mItemClickListener;
+    private Context mContext;
+    private String imageSize = "w185/";
+    private static final String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/";
 
-    public MovieAdapter(@NonNull Context context, @NonNull List<Movie> movies) {
-        super(context, 0, movies);
+    public MovieAdapter(Context context, List<Movie> movies) {
+        mContext = context;
         mMovies = movies;
     }
 
-    @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        ImageView imageView;
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        mInflater = LayoutInflater.from(mContext);
+        View view = mInflater.inflate(R.layout.recyclerview_item, parent, false);
+        return new ViewHolder(view);
+    }
 
-        if (convertView == null){
-            convertView = LayoutInflater.from(getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
-            imageView = (ImageView) convertView;
-            } else {
-            imageView = (ImageView) convertView;
-        }
+    @Override
+    public void onBindViewHolder(MovieAdapter.ViewHolder holder, int position) {
 
         Picasso.with(mContext)
                 .load(BASE_IMAGE_URL + imageSize + mMovies.get(position).getPoster())
-                .into(imageView);
+                .into(holder.posterImageView);
+    }
 
-        return imageView;
+    @Override
+    public int getItemCount() {
+        return mMovies.size();
+    }
+
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView posterImageView;
+
+        private ViewHolder(View itemView) {
+            super(itemView);
+
+            posterImageView = itemView.findViewById(R.id.poster_image_view);
+        }
+}
+
+    public void setMovieData(List<Movie> movies) {
+        mMovies = movies;
+        notifyDataSetChanged();
     }
 }
+
